370ec8506631797f3ab86f76a6662c19d653ee98
Raoul Vos <raoul.vos@munisense.com>
2020-02-11 10:00:26 +0100

    Y2015D2P2 complete

--- src/Y_2015/Y2015D2.php
+++ src/Y_2015/Y2015D2.php
@@ -14,21 +14,7 @@ class Y2015D2 extends Day
         $input = $this->getInputArray(true, "x");
 
         if ($this->test) {
-            [$length, $width, $height] = reset($input);
-            $solution = $this->calculateWrappingPaperRequired((int) $length, (int) $width, (int) $height);
-            if ($solution !== 58) {
-                $output->writeln("Test1 Failed! Wrapping paper required should have been 58 but was: " . $solution);
-            } else {
-                $output->writeln("Test1 success");
-            }
-
-            [$length, $width, $height] = end($input);
-            $solution = $this->calculateWrappingPaperRequired((int) $length, (int) $width, (int) $height);
-            if ($solution !== 43) {
-                $output->writeln("Test2 Failed! Wrapping paper required should have been 43 but was: " . $solution);
-            } else {
-                $output->writeln("Test2 success");
-            }
+            $this->testAssignment1($input, $output);
         } else {
             $total_sq_feet = 0;
             foreach ($input as [$length, $width, $height]) {
@@ -41,37 +27,65 @@ class Y2015D2 extends Day
 
     public function runAssignment2(OutputInterface $output): void
     {
-        $fuel_requirement = 0;
-        $handle = fopen($this->input_file, "rb");
-        if ($handle) {
-            while (($line = fgets($handle)) !== false) {
-                $total_fuel_requirement_for_module = 0;
-                $input_mass = (int) $line;
-                while ($input_mass > 0) {
-                    $fuel_requirement_for_module = $this->calculateFuelNeeded($input_mass);
-                    if ($fuel_requirement_for_module < 0) {
-                        $fuel_requirement_for_module = 0;
-                        $input_mass = 0;
-                    } else {
-                        $input_mass = $fuel_requirement_for_module;
-                    }
+        $input = $this->getInputArray(true, "x");
 
-                    $total_fuel_requirement_for_module += $fuel_requirement_for_module;
-                }
-
-                $output->writeln("Module: Total fuel needed: " . $total_fuel_requirement_for_module);
-                $fuel_requirement += $total_fuel_requirement_for_module;
+        if ($this->test) {
+            $this->testAssignment2($input, $output);
+        } else {
+            $total_sq_feet = 0;
+            foreach ($input as [$length, $width, $height]) {
+                $total_sq_feet += $this->calculateRibbonRequired((int) $length, (int) $width, (int) $height);
             }
 
-            $output->writeln("P2: Total fuel needed: " . $fuel_requirement);
-
-            fclose($handle);
-        } else {
-            $output->writeln("Error reading line input from file");
+            $output->writeln("Total sq feet of ribbon they should order: " . $total_sq_feet);
         }
     }
 
-    public function calculateWrappingPaperRequired(int $length, int $width, int $height): int
+    private function testAssignment1(array $input, OutputInterface $output): void
+    {
+        [$length, $width, $height] = reset($input);
+        $solution = $this->calculateWrappingPaperRequired((int) $length, (int) $width, (int) $height);
+        if ($solution !== 58) {
+            $output->writeln("P1 Test1 Failed! Wrapping paper required should have been 58 but was: " . $solution);
+        } else {
+            $output->writeln("P1 Test1 success");
+        }
+
+        [$length, $width, $height] = end($input);
+        $solution = $this->calculateWrappingPaperRequired((int) $length, (int) $width, (int) $height);
+        if ($solution !== 43) {
+            $output->writeln("P1 Test2 Failed! Wrapping paper required should have been 43 but was: " . $solution);
+        } else {
+            $output->writeln("P1 Test2 success");
+        }
+    }
+    private function testAssignment2(array $input, OutputInterface $output): void
+    {
+        [$length, $width, $height] = reset($input);
+        $solution = $this->calculateRibbonRequired((int) $length, (int) $width, (int) $height);
+        if ($solution !== 34) {
+            $output->writeln("P2 Test1 Failed! Ribbon required should have been 34 but was: " . $solution);
+        } else {
+            $output->writeln("P2 Test1 success");
+        }
+
+        [$length, $width, $height] = end($input);
+        $solution = $this->calculateRibbonRequired((int) $length, (int) $width, (int) $height);
+        if ($solution !== 14) {
+            $output->writeln("P2 Test2 Failed! Ribbon required should have been 14 but was: " . $solution);
+        } else {
+            $output->writeln("P2 Test2 success");
+        }
+    }
+
+    private function calculateRibbonRequired(int $length, int $width, int $height): int
+    {
+        $inputs = [$length, $width, $height];
+        sort($inputs);
+        return ($inputs[0] + $inputs[0] + $inputs[1] + $inputs[1]) + ($length * $width * $height);
+    }
+
+    private function calculateWrappingPaperRequired(int $length, int $width, int $height): int
     {
         return (2*$length*$width) + (2*$width*$height) + (2*$height*$length) + min([($length*$width),($width*$height),($height*$length)]);
     }
