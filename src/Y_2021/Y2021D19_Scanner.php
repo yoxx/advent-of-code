db3470e1454f1c897a8ed710c620c670c306226d
Raoul Vos <raoul.vos@munisense.com>
2021-12-19 14:30:11 +0100

    WIP D19

--- src/Y_2021/Y2021D19_Scanner.php
+++ src/Y_2021/Y2021D19_Scanner.php
@@ -28,22 +28,76 @@ class Y2021D19_Scanner
             foreach ($this->beacons as $ref_beacon) {
                 foreach ($other_scanner->beacons as $oth_beacon) {
                     // Compare the ref beacon with the origin beacon x,y,z
-                    if (EH?!) {
-                        // we have a match thus add it to the array above
-                        $compare_orientation[implode(",",$orientation)][] = $oth_beacon;
+                    // Perhaps its better to calculate the scanner position.
+                    // If we know the position... We can count the times a position is matched
+                    // HOW DO I CALCULTATE THE POSITION OF A SCANNER?
+                    // Positions are relative to the scanner, but we do know position of the ref beacon
+                    // What if we simply calculate for ALL beacons asuming they are on the same position?
+                    // If they are above 12 im probably right?
+                    [$ob_x, $ob_y, $ob_z] = $this->handleRotation($oth_beacon->x, $oth_beacon->y, $oth_beacon->z, $orientation);
+
+                    // Im not sure why my other scanner position is incorrect. Need to debug.
+                    $other_scanner_pos = [$ob_x + $ref_beacon->x, $ob_y + $ref_beacon->y, $ob_z + $ref_beacon->z];
+                    if (!isset($compare_orientation[implode(",", $orientation)][implode(",", $other_scanner_pos)])) {
+                        $compare_orientation[implode(",", $orientation)][implode(",", $other_scanner_pos)] = 1;
+                    } else {
+                        $compare_orientation[implode(",", $orientation)][implode(",", $other_scanner_pos)]++;
                     }
                 }
             }
             // Check if we found 12 or more matches
-            if (count($compare_orientation[implode(",",$orientation)]) >= 12) {
-                // In this orientation atleast 12 beacons match
-                // Set position? where do I get the other scanner position from?
+            foreach($compare_orientation[implode(",",$orientation)] as $scanner_pos => $potential_match) {
+                if ($potential_match >= 12) {
+                    // In this orientation atleast 12 beacons match
+                    // Set position? where do I get the other scanner position from?
 //                $other_scanner->alignToReference();
-                return $other_scanner;
+                    return $other_scanner;
+                }
             }
         }
-        // Well if we end up here we simply found no match
+        // Well if we end up here we simply found no absolute location
         return null;
+    }
+
+    public function handleRotation(int $x, int $y, int $z, array $orientation): array
+    {
+        if ($orientation["x"] !== 0) {
+            [$x, $y, $z] = $this->rotateX($orientation["x"], $x, $y, $z);
+        }
+        if ($orientation["y"] !== 0) {
+            [$x, $y, $z] = $this->rotateY($orientation["y"], $x, $y, $z);
+        }
+        if ($orientation["z"] !== 0) {
+            [$x, $y, $z] = $this->rotateZ($orientation["z"], $x, $y, $z);
+        }
+        return [$x, $y, $z];
+    }
+
+    public function rotateX(int $direction, int $x, int $y, int $z): array
+    {
+        // returning x,y,z
+        if ($direction === 1) {
+            return [$x, $z*-1, abs($y)];
+        }
+        return [$x, abs($z), $y-1];
+    }
+
+    public function rotateY(int $direction, int $x, int $y, int $z): array
+    {
+        // returning x,y,z
+        if ($direction === 1) {
+            return [$z *-1, $y, abs($x)];
+        }
+        return [abs($z), $y, $x*-1];
+    }
+
+    public function rotateZ(int $direction, int $x, int $y, int $z): array
+    {
+        // returning x,y,z
+        if ($direction === 1) {
+            return [abs($y), $x*-1, $z];
+        }
+        return [$y*-1, abs($x), $z];
     }
 
     public function alignToReference(Y2021D19_Scanner $reference_scanner): void
