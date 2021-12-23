479e5dcc023feba53443e2025d8d2c25768172e1
Raoul Vos <admin@yoxxtech.nl>
2021-12-23 07:19:55 +0100

    WIP D22

--- src/Y_2021/Y2021D22.php
+++ src/Y_2021/Y2021D22.php
@@ -17,21 +17,20 @@ class Y2021D22 extends Day
 
     public function runAssignment2(OutputInterface $output): void
     {
-        Utils::memoryIntensive16G();
         $input = $this->parseInput();
-        $matrix = $this->runInstructions($output, $input, false);
+        $matrix = $this->runInstructionsP2($input);
         $output->writeln("P2: The solution is: " . array_count_values($matrix)[1]);
     }
 
-    public function runInstructions(OutputInterface $output, array $instructions, bool $enforce_bounds = true): array
+    public function runInstructions(OutputInterface $output, array $instructions): array
     {
         $matrix = [];
         foreach($instructions as $reboot_operation) {
             $output->writeln(implode(",", [$reboot_operation["operation"], implode("-", $reboot_operation["x"]), implode("-", $reboot_operation["y"]), implode("-", $reboot_operation["z"])]));
             // check bounds
-            if ($enforce_bounds && ($reboot_operation["x"][0] < -50 || $reboot_operation["x"][1] > 50 ||
+            if ($reboot_operation["x"][0] < -50 || $reboot_operation["x"][1] > 50 ||
                 $reboot_operation["y"][0] < -50 || $reboot_operation["y"][1] > 50 ||
-                $reboot_operation["z"][0] < -50 || $reboot_operation["z"][1] > 50)) {
+                $reboot_operation["z"][0] < -50 || $reboot_operation["z"][1] > 50) {
                 continue;
             }
             for ($x = $reboot_operation["x"][0]; $x <= $reboot_operation["x"][1]; $x++) {
@@ -43,6 +42,39 @@ class Y2021D22 extends Day
             }
         }
         return $matrix;
+    }
+
+    public function runInstructionsP2(array $instructions): array
+    {
+        $cuboid_list = [];
+        // Fill our initial list
+        foreach($instructions as $intersection_cuboid) {
+            $new_cubiod_list = [];
+            // We check if we have an intersection. between cuboid a/b
+            foreach($cuboid_list as $cubiod_list_item) {
+                // There is an intersect when cube A x,y and z are within cube B's x,y, and z there is an intersect
+                if ((($intersection_cuboid["x"][0] >= $cubiod_list_item["x"][0] && $intersection_cuboid["x"][0] <= $cubiod_list_item["x"][1]) ||
+                    ($intersection_cuboid["x"][1] >= $cubiod_list_item["x"][0] && $intersection_cuboid["x"][1] <= $cubiod_list_item["x"][1])) &&
+                    (($intersection_cuboid["y"][0] >= $cubiod_list_item["y"][0] && $intersection_cuboid["y"][0] <= $cubiod_list_item["y"][1]) ||
+                    ($intersection_cuboid["y"][1] >= $cubiod_list_item["y"][0] && $intersection_cuboid["y"][1] <= $cubiod_list_item["y"][1])) &&
+                    (($intersection_cuboid["z"][0] >= $cubiod_list_item["z"][0] && $intersection_cuboid["z"][0] <= $cubiod_list_item["x"][1]) ||
+                    ($intersection_cuboid["z"][1] >= $cubiod_list_item["z"][0] && $intersection_cuboid["z"][1] <= $cubiod_list_item["x"][1]))) {
+                    // We have an intersection!!
+                    $new_cubiod_list[] = [
+                        "operation" => $intersection_cuboid["operation"],
+                        "x" => [min($intersection_cuboid["x"][0], $cubiod_list_item["x"][0]), max($intersection_cuboid["x"][1], $cubiod_list_item["x"][1])],
+                        "y" => [min($intersection_cuboid["y"][0], $cubiod_list_item["y"][0]), max($intersection_cuboid["y"][1], $cubiod_list_item["y"][1])],
+                        "z" => [min($intersection_cuboid["z"][0], $cubiod_list_item["z"][0]), max($intersection_cuboid["z"][1], $cubiod_list_item["z"][1])],
+                    ];
+                }
+            }
+            if (empty($new_cubiod_list)) {
+                $new_cubiod_list[implode(",", [implode("-", $intersection_cuboid["x"]), implode("-", $intersection_cuboid["y"]), implode("-", $intersection_cuboid["z"])])] = $intersection_cuboid;
+            }
+            // Set the new items as cubiod list
+            $cuboid_list = array_merge($cuboid_list, $new_cubiod_list);
+        }
+        return $cuboid_list;
     }
 
     public function parseInput(): array
